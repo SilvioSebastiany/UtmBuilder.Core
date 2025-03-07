476baac2a6d1b5d30967995fa8230f45b1c5b43e
Silvio Sebastiany <silvio.sebastiany.gmail.com>
2025-03-06 21:56:39 -0300

    😊 Optional Parameters - Add Campains value object with properties for source, medium, name, and optional attributes

--- UtmBuilder.Core/ValueObjects/Campains.cs
+++ UtmBuilder.Core/ValueObjects/Campains.cs
@@ -2,6 +2,28 @@ namespace UtmBuilder.Core.ValueObjects
 {
     public class Campains : ValueObject
     {
-        // Class implementation goes here
+        public string Source { get; }
+        public string Medium { get; }
+        public string Name { get; }
+        public string? Id { get; set; }
+        public string? Term { get; set; }
+        public string? Content { get; set; } 
+
+
+        public Campains(
+            string source, 
+            string medium, 
+            string name,
+            string? id = null,
+            string? term = null,
+            string? content = null)
+        {
+            Source = source;
+            Medium = medium;
+            Name = name;
+            Id = id;
+            Term = term;
+            Content = content;
+        }
     }
 }
