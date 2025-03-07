738eb4052438872f5ce9e57bd4252ce367056099
Silvio Sebastiany <silvio.sebastiany.gmail.com>
2025-03-06 22:15:04 -0300

    👌 Summary - Enhance Campains and Url value objects: add properties for campaign details and enforce URL immutability

--- UtmBuilder.Core/ValueObjects/Campains.cs
+++ UtmBuilder.Core/ValueObjects/Campains.cs
@@ -2,11 +2,45 @@ namespace UtmBuilder.Core.ValueObjects
 {
     public class Campains : ValueObject
     {
+        /// <summary>
+        /// Generate a new Campains for a URL
+        /// </summary>
+        /// <param name="source">The referrer (e.g. google, newsletter)</param>
+        /// <param name="medium">Marketing medium (e.g. cpc, banner, email)</param>
+        /// <param name="name">Product, promo code, or slogan (e.g. spring_sale)</param>
+        /// <param name="id">The ads campaign id</param>
+        /// <param name="term">Identify the paid keywords</param>
+        /// <param name="content">Use to differentiate similar content, or links within the same ad</param>
+
+
+        /// <summary>
+        /// The referrer (e.g. google, newsletter)
+        /// </summary>
         public string Source { get; }
+
+        /// <summary>
+        /// Marketing medium (e.g. cpc, banner, email)
+        /// </summary>
         public string Medium { get; }
+
+        /// <summary>
+        /// Product, promo code, or slogan (e.g. spring_sale)
+        /// </summary>
         public string Name { get; }
+
+        /// <summary>
+        /// The ads campaign id
+        /// </summary>
         public string? Id { get; set; }
+
+        /// <summary>
+        /// Identify the paid keywords
+        /// </summary>
         public string? Term { get; set; }
+
+        /// <summary>
+        /// Use to differentiate similar content, or links within the same ad   
+        /// </summary>
         public string? Content { get; set; } 
 
 
--- UtmBuilder.Core/ValueObjects/Url.cs
+++ UtmBuilder.Core/ValueObjects/Url.cs
@@ -2,14 +2,23 @@ namespace UtmBuilder.Core.ValueObjects
 {
     public class Url : ValueObject
     {
+        /// <summary>
+        /// Address of URL (Website link)
+        /// </summary>
         public string Address { get; }
 
         // set foi removido, porque o endereço de uma URL não deve ser alterado
         // só pode ser definido no construtor
 
+        /// <summary>
+        /// Create a new URL
+        /// </summary>
+        /// <param name="address">Address of URL (Website link)</param>
         public Url(string address)
         {
             Address = address;
         }
+
+
     }
 }
