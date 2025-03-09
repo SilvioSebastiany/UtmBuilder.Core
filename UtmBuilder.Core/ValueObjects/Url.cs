609b65ae2079b43ce38693a037d20b7df5bb1221
Silvio Sebastiany <silvio.sebastiany.gmail.com>
2025-03-09 15:15:00 -0300

    ✨  Utilizando Regex - Validate URL format in Url value object constructor: add regex pattern and throw exception for invalid URLs

--- UtmBuilder.Core/ValueObjects/Url.cs
+++ UtmBuilder.Core/ValueObjects/Url.cs
@@ -2,6 +2,8 @@ namespace UtmBuilder.Core.ValueObjects
 {
     public class Url : ValueObject
     {
+        private const string UrlRegexPattern = @"^((http|ftp|https|www)://)?([\w+?\.\w+])+([a-zA-Z0-9\~\!\@\#\$\%\^\&\*\(\)_\-\=\+\\\/\?\.\:\;\'\,]*)?$";
+";
         /// <summary>
         /// Address of URL (Website link)
         /// </summary>
@@ -17,6 +19,11 @@ namespace UtmBuilder.Core.ValueObjects
         public Url(string address)
         {
             Address = address;
+            
+            if(Regex.IsMatch(address, UrlRegexPattern))
+            {
+                throw new ArgumentException("Invalid URL");
+            }
         }
 
 
