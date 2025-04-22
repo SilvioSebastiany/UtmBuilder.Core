acd4e41cd2cad525b5b3315b0e0ea2e9a7e6999e
Silvio Sebastiany <silvio.sebastiany.gmail.com>
2025-04-22 14:47:50 -0300

    Convertendo a URL

--- UtmBuilder.Core/Entity/Utm.cs
+++ UtmBuilder.Core/Entity/Utm.cs
@@ -1,5 +1,6 @@
 using UtmBuilder.Core.Extensions;
 using UtmBuilder.Core.ValueObjects;
+using UtmBuilder.Core.ValueObjects.Exceptions;
 
 namespace UtmBuilder.Core.Entity
 {
@@ -25,6 +26,32 @@ namespace UtmBuilder.Core.Entity
         // converte o objeto Utm para string
         // o operador implicit permite que o objeto Utm seja convertido para string automaticamente
         public static implicit operator string(Utm utm) => utm.ToString();
+
+        public static implicit operator Utm(string utmString)
+        {
+           if(string.IsNullOrEmpty(utmString))
+                throw new InvalidUrlException("Invalid URL");
+
+            var url = new Url(utmString);
+            var segments = url.Address.Split('?');
+            if (segments.Length < 2)
+                throw new InvalidUrlException("No segments found in URL");
+
+            var pars = segments[1].Split('&');
+            var source = pars.Where(p => p.StartsWith("utm_source")).FirstOrDefault("").Split('=')[1];
+            var medium = pars.Where(p => p.StartsWith("utm_medium")).FirstOrDefault("").Split('=')[1];
+            var name = pars.Where(p => p.StartsWith("utm_campaign")).FirstOrDefault("").Split('=')[1];
+            var id = pars.Where(p => p.StartsWith("utm_id")).FirstOrDefault("").Split('=')[1];
+            var term = pars.Where(p => p.StartsWith("utm_term")).FirstOrDefault("").Split('=')[1];
+            var content = pars.Where(p => p.StartsWith("utm_content")).FirstOrDefault("").Split('=')[1];
+
+            var utm = new Utm(
+                new Url(segments[0]), 
+                new Campains(source, medium, name, id, term, content));
+
+            return utm;
+            
+        }
         
 
         public override string ToString()
